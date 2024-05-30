e002797c88ecd7193bbae5331fda5afc7944ee49
Julie Raymond <julie.raymond@pichet.com>
2024-05-30 16:34:07 +0200

    homepage component image header done responsive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {ButtonComponent} from "./button/button.component";
 import {MatIconModule} from "@angular/material/icon";
 import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component";
 import {FooterComponent} from "./footer/footer.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, MatIconModule, NavigationBarComponent, FooterComponent],
+  imports: [RouterOutlet, ButtonComponent, MatIconModule, NavigationBarComponent, FooterComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {ButtonComponent} from "../button/button.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+  homepagePathImage ="/assets/images/homepage-mobil.jpg";
 
+  conceptText: string = "Alice et Anne, surnommées les '2 A', partagent une passion pour la course à pied. Constatant que beaucoup hésitent à se lancer dans ce sport, elles décident d'organiser des courses gratuites, encourageant ami.e.s et proches à se joindre à elles.";
+  //
+  // value1Title: string = "Entraite !";
+  // value1Description: string = "Chacun se soutient et s’encourage pour donner le meilleur de soi et atteindre le but qu’il s’est fixé !";
+  //
+  // value2Title: string = "Sourire !";
+  // value2Description: string = "Des courses…mais aussi des apéros organisés par l’association !";
+  //
+  // value3Title: string = "Solidarité !";
+  // value3Description: string = "2€ sur l’adhésion annuelle sont reversés à une association caritative.";
+
+  constructor(private router: Router) {}
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
