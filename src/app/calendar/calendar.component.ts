83676341ce0a26dd58df6e8a20da6078dcfe6b11
Julie Raymond <julie.raymond@pichet.com>
2024-05-31 12:14:16 +0200

    stepper for booking a course done - css to do for colors/fonts/size

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -2,20 +2,28 @@ import {Component} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {provideNativeDateAdapter} from '@angular/material/core';
+import {Course} from "../interfaces/course.interface";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {MatStep, MatStepper} from "@angular/material/stepper";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
   providers: [provideNativeDateAdapter()],
   imports: [
-    MatCardModule, MatDatepickerModule
+    MatCardModule, MatDatepickerModule, NgIf, MatButton, NgForOf, MatStepper, MatStep
   ],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 
 export class CalendarComponent {
-  selected: Date | null = null;
+  isLinear = false;
+  selectedDate: Date | null = null;
+  selectedHour: string | null = null;
+  selectedCourse: Course | null = null;
+  stepperIndex: number = 0; // Pour suivre l'index de l'étape actuelle
 
   formatDateToFrench(selectedDate: Date): string {
     const options: Intl.DateTimeFormatOptions = {
@@ -25,5 +33,28 @@ export class CalendarComponent {
       day: 'numeric'
     };
     return selectedDate.toLocaleDateString('fr-FR', options);
+  }
+
+  // Objet fictif représentant un exemple de cours
+  courseExemple: Course = {
+    courseID: 1,
+    title: "Sprint",
+    description: "Description Course 1",
+    date: new Date(2024, 5, 31),
+    available_hours: ['09:00', '10:00', '11:00'],
+    level: "Groupe orange",
+    kilometer: "8km",
+    time: "35min"
+  };
+
+  // Méthode pour avancer à l'étape suivante du stepper
+  nextStep(): void {
+    this.stepperIndex++;
+  }
+
+  // Méthode pour réserver le cours
+  reserveCourse(): void {
+    // Implémentez ici la logique de réservation
+    console.log('Course réservée:', this.selectedCourse);
   }
 }
---
+++ src/app/interfaces/course.interface.ts
@@ -0,0 +1,10 @@
+export interface Course {
+  courseID: number;
+  title: string;
+  description: string;
+  date: Date;
+  available_hours: string[];
+  level: string;
+  kilometer: string;
+  time: string;
+}
