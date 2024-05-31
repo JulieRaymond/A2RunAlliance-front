e20a2e070f9bd6be0b8e3ebef6e8f7d63431ea64
Julie Raymond <julie.raymond@pichet.com>
2024-05-31 10:25:17 +0200

    calendar to french

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,16 @@ import {MatIconModule} from "@angular/material/icon";
 import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component";
 import {FooterComponent} from "./footer/footer.component";
 import {HomepageComponent} from "./homepage/homepage.component";
+import {MAT_DATE_LOCALE} from "@angular/material/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ButtonComponent, MatIconModule, NavigationBarComponent, FooterComponent, HomepageComponent],
-  templateUrl: './app.component.html',
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},
+  ],
+    templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
